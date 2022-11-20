bb90421fa9ea604342a747636490c505adaf3f3b
JScruzs <jscruz234@misena.edu.co>
2022-11-19 20:33:31 -0500

    Se comenta el archivo Food para especificar las funciones del codigo

--- food.py
+++ food.py
@@ -1,5 +1,18 @@
+# Se importa la libreria turtle y se importa la funcion random 
+# la cual es para que un elemento sea al azar.
+
 from turtle import Turtle
 import random
+
+# Se crea la clase Food la cual se define para que
+# se cree los elementos de la comida definiendoles el color,
+# la forma, la velocidad en la que tiene que aparecer y el tamaño
+# para que aparesca de manera aleatoria se definen las variables
+# random_x para los limites horizontales y random_y para los 
+# limites verticales a las cuales se les pone un limite de
+# posiciones de -280 pixeles y 280 pixeles a lo que se refiere
+# es que los elementos no pueden aparecer en ningun lugar fuera
+# de esos limites definidos
 
 class Food(Turtle):
     def __init__(self):
